1497db1b8cd24c42cb55324ded80024f5b8931fd
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-25 12:11:50 +0100

    skeleton

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { evaluate } from 'mathjs'
+import './App.css'
+
+const App = () => {
+  const [sum, setSum] = useState("")
+  const buttons = ["1", "2", "3", "4","5", "6", "7", "8","9", "0","-", "+","*", "/", "="]
+  const handleClick = (btn) => {
+   // If user enters =
+   // Pass the sum into the evaluate function
+   // The Evaluate answer becomes sum after setSum is given the above
+   console.log(btn)
+ }
+  return (
+     <div className="centre">
+       <h2>Kyle's Below Adverage Calculator</h2>
+       <h4 className="inputBox">{sum}</h4>
+       <div className="buttonWrap">
+         {
+           buttons.map((button, index) => {
+             return (
+               <button key={index} onClick={() => handleClick(button)} className="btn">{button}</button>
+             )
+           })
+         }
+       </div>
+     </div>
+ )
+}
+export default App
+ 
---
+++ src/math.js
@@ -0,0 +1,21 @@
+let add = (num1, num2) => {
+    let result = num1 + num2;
+    return result;
+};
+
+let subtract = (num1, num2) => {
+    let result = num1 - num2;
+    return result;
+};
+
+let multiply = (num1, num2) => {
+    let result = num1 * num2;
+    return result;
+};
+
+let divide = (num1, num2) => {
+    let result = num1 / num2;
+    return result;
+};
+
+module.exports = {add, subtract, multiply, divide};
